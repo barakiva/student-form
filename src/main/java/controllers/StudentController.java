83e0e9f4166adb46cd6f4746288ad20b479223e8
Bar Akiva <barakiva1@gmail.com>
2020-01-09 12:41:56 +0200

    root form

--- src/main/java/controllers/StudentController.java
+++ src/main/java/controllers/StudentController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,12 +17,11 @@ import services.CityService;
 
 @Controller
 @ComponentScan("services")
-@RequestMapping("/students")
+@RequestMapping("/")
 public class StudentController {
 	@Autowired
 	private CityService cityService;
-	
-	@RequestMapping("/show-form")
+	@GetMapping
 	public String showForm(Model theModel) {
 
 		// create a student object
